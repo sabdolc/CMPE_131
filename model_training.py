094e9c9150a64fa9ffc30391fd21a113c6cf5eb6
HelloThere1019 <jacob.a.thomas@sjsu.edu>
2025-05-03 19:23:05 -0700

    added tokenizer

--- model_training.py
+++ model_training.py
@@ -1,8 +1,19 @@
 import pandas as pd
+import nltk
+nltk.download('punkt_tab')
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TFidfVectorizer
 
 #get data set
 df = pd.read_csv("spotify_millsongdata.csv") #read data
 df = df.sample(5000).drop('link', axis=1).reset_index(drop=True) #pick 5000 random samples and remove link column
 
 #text preprocessing
-df['text'] = df['text'].str.lower().replace(r'^\w\s', ' ', regex = True).replace(r'\n', ' ', regex = True) #DOUBLE CHECK
+df['text'] = df['text'].str.lower().replace(r'^\w\s', ' ', regex = True).replace(r'\n', ' ', regex = True).replace(r'\r', ' ', regex = True) #DOUBLE CHECK
+
+stemmer = PorterStemmer()
+
+def token(text):
+    token = nltk.word_tokenize(text)
+    list = [stemmer.stem(i) for i in token]
+    return " ".join(list)
